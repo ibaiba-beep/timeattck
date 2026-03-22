d456bfea1cdc56aadfb0f1f7e285e2407e4d9fb2
kwak <iba1008@gmail.com>
2026-03-22 09:23:29 +0900

    Initial Commit

---
+++ timeattck/timeattckApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeattckApp.swift
+//  timeattck
+//
+//  Created by 곽승민 on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct timeattckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
